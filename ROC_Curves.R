8517fe3baa31d4ec597e45e37fae98a9d72ffafb
Ilya Kukovitskiy <37713804+Ilya-Kuk@users.noreply.github.com>
2018-06-12 20:17:28 -0400

    Add files via upload

--- ROC_Curves.R
+++ ROC_Curves.R
@@ -1,10 +1,24 @@
 require(ROCR)
 
-rocplot = function(pred, truth, ...){
+rocplot_p = function(pred, truth, ...){
   predob = prediction(pred, truth)
   perf = performance(predob, "tpr", "fpr")
   plot(perf,...)
 }
 
-fitted = predict(glm.prf,type='response')
+rocplot_n = function(pred, truth, ...){
+  predob = prediction(pred, truth)
+  perf = performance(predob, "tnr", "fnr")
+  plot(perf,...)
+}
+
+fitted = predict(glm.prr,type='response')
 rocplot(fitted, post_retail_response)
+rocplot(fitted, na.omit(d)$post_retail_response) #must na.omit transform the data first - only 3271 entries
+
+fitted1 = predict(glm.prr1,type='response')
+rocplot(fitted1, na.omit(d)$post_retail_response) #must na.omit transform the data first - only 3271 entries
+
+fitted2 = predict(glm.prr2,type='response')
+rocplot(fitted2, na.omit(d)$post_retail_response) #must na.omit transform the data first - only 3271 entries
+
